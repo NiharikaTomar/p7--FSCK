6524a6d12e76608a90dfa28b239b8b39e8a2d63a
Niharika Tomar <tomar@royal-18.cs.wisc.edu>
2019-12-09 17:38:33 -0600

    pc dying

--- part_b/fscheck.c
+++ part_b/fscheck.c
@@ -11,69 +11,100 @@
 #include "stat.h"
 #include "param.h"
 
-void *img;
-struct superblock *sblock;
-struct dinode *di;
-char bitmap[8];
-void *datablock;
-
+//SANITY CHECK - MAP
+// block 0 = unused
+// block 1: superblock
+// structure :
+// 0.unused --> 1.superblock -->2.inodetable --> 3.bitmap --> 4.data blocks
 
 int main(int argc, char *argv[]){
-	// sblock = (img + BSIZE);
+	// DECLARATION
+	void *img;
+	struct superblock *sblock;
+	struct dinode *di;
+	struct dirent *dirent;
+	struct stat *stat;
+	int bitmap[sblock->size]; // bitmap array
+	int numBlocks; // number of data numBlocks
+
+
+	// INITIALIZATION
+	sblock = (img + BSIZE);
+
 	if(argc < 2 || argc > 2){
 		printf("illegal number of arguments");
 		fflush();
 	}
 	int fd;
 	fd = open(argv[1], O_RDONLY);
-  if (fd == -1) {
+  if (fd < 0) {
     printf(stderr, "ERROR: image not found\n");
 		fflush();
     exit(1);
   }
-	// img = mmap();
+
+	numBlocks = (sblock->ninodes * sizeof(struct dinode))/ BSIZE + 1;
+	// img = mmap(NULL, stat->size, , , fd, 0);
 	sblock = (img + BSIZE);
 	di = (BSIZE * 2 + img)) // inode table starts at index 2
-	// block 0 = unused
-	// block 1: superblock
- 	// structure :
- 	// 0.unused --> 1.superblock -->2.inodetable --> 3.bitmap --> 4.data blocks
+
 	// Since bitmap starts at index 3,
 	bitmap = (BSIZE * (sblock->ninodes/IPB + 3) + img);
 	// Since datablock starts at index 4,
-	// datablock = (BSIZE * (sblock->ninodes/IPB + .... + 4) + img);
+	// numBlocks = (BSIZE * (sblock->ninodes/IPB + .... + 4) + img);
 
 	// CHECKS
 	// DIRECTORY CHECKS
 	// check 1 : root dir
-  if ((ROOTINO + di)->type != T_DIR) {  //a)
+  if ((ROOTINO + di)->type != T_DIR) {  //a) if type not directory
     printf(stderr, "ERROR: root directory does not exist.\n");
 		fflush();
     exit(1);
   }
-	// if (!(ROOTINO + di)){ //b
+	// if (!(ROOTINO + di)){ // b) if it doesn't even exist
 	// 	printf(stderr, "ERROR: root directory does not exist.\n");
 	// 	fflush();
-  //   exit(1);
+  //  exit(1);
 	// }
 
 	// check 2 : current dir mismatch ???
 
 	// INODE CHECKS
 	// check 1 : bad inode
-	if (di->type != T_DIR
-		//&& di->type != T_SYM
-    && di->type != T_FILE
-		&& di->type != T_DEV) {
-    printf(stderr, "ERROR: bad inode.\n");
-		fflush();
-    exit(1);
-	}
+	for (int i = 0; i < sblock->ninodes; i++){
+		if (di->type != T_DIR
+			//&& di->type != T_SYM
+	    && di->type != T_FILE
+			&& di->type != T_DEV
+			&& di->type != 0) {
+	    printf(stderr, "ERROR: bad inode.\n");
+			fflush();
+	    exit(1);
+		}
+}
 	// check 2 : bad size ???
 
 	// BITMAP CHECKS
 	// check 1 : bitmap marks data free
+	for (int i = 0; i < NDIRECT+1; i++){
+		if (bitmap[di->addrs[i]] <= 0)
+		printf(stderr,"ERROR: bitmap marks data free but data block used by inode.\n");
+		fflush();
+		exit(1);
+	}
 
+	// check 2 : bitmap marks data in use
+	for (int i = 0; i < sblock->size; i++){
+		if (i > numBlocks && bitmap[i] > 0) { // ????
+			printf(stderr,"ERROR: bitmap marks data block in use but not used.\n");
+			fflush();
+			exit(1);
+		}
+	}
+
+	// size of bitmap?
+	int size;
+	// size = (sblock->nblocks + sblock->ninodes/IPB + sblock->nblocks/BPB + 4) / 8;
 
 
 
